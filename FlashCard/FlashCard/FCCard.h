865055458dde3ed114879d7c5eb15b4653d0c197
yico <yico2048@icloud.com>
2014-01-06 21:35:12 +0800

    add FCCard Classes

--- FlashCard/FlashCard/FCCard.h
+++ FlashCard/FlashCard/FCCard.h
@@ -10,4 +10,19 @@
 
 @interface FCCard : NSObject
 
+@property(nonatomic) bool isCorrect;
+@property(nonatomic) int correctAnswer;
+
+@property(nonatomic,strong) NSMutableArray* answers;
+@property(nonatomic,strong) NSString *cardText;
+
+-(id) initWithQuestion:(NSString*)question
+                 answer:(NSString*)answer
+           wrongAnswer1:(NSString*)wrongAnswer1
+           wrongAnswer2:(NSString*)wrongAnswer2;
+
+-(void) recordAnswer:(int)answerNum;
+
+//Excerpt From: Lou Franco, Eitan Mendelowitz. “Hello iOS Development v14.” iBooks.
+
 @end
--- FlashCard/FlashCard/FCCard.m
+++ FlashCard/FlashCard/FCCard.m
@@ -10,4 +10,36 @@
 
 @implementation FCCard
 
+-(id) initWithQuestion:(NSString*)question
+                 answer:(NSString*)answer
+           wrongAnswer1:(NSString*)wrongAnswer1
+           wrongAnswer2:(NSString*)wrongAnswer2
+{
+    if (self = [super init]) {
+        self.cardText = question;
+        
+        self.answers = [[NSMutableArray alloc]
+                        init];
+        
+        [self.answers addObject:answer];
+        [self.answers addObject:wrongAnswer1];
+        [self.answers addObject:wrongAnswer2];
+        
+        int randomAnswerSlot = arc4random() % 3;
+        [self.answers exchangeObjectAtIndex:0
+                          withObjectAtIndex:randomAnswerSlot];
+        self.correctAnswer = randomAnswerSlot;
+    }
+    return self;
+}
+
+//Excerpt From: Lou Franco, Eitan Mendelowitz. “Hello iOS Development v14.” iBooks.
+
+-(void) recordAnswer:(int)answerNum
+{
+    self.isCorrect = (answerNum == self.correctAnswer);
+}
+
+//Excerpt From: Lou Franco, Eitan Mendelowitz. “Hello iOS Development v14.” iBooks.
+
 @end
