15baa5b670c4a2145398b01588cce7ac908b0d7e
mtrove <yico1989@gmail.com>
2015-10-25 19:00:02 +0800

    change it later

---
+++ Calculator/Calculator/Fraction.h
@@ -0,0 +1,20 @@
+//
+//  Fraction.h
+//  Calculator
+//
+//  Created by Near on 10/24/15.
+//  Copyright © 2015 Near. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+
+-(void) print;
+
+-(void) setNum:(int)n;
+
+-(void) setDem:(int)d;
+
+
+@end
